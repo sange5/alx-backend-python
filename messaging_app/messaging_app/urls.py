9c84fc7d41f4833d12bd564049be80c51f075d9e
sange5 <163669819+sange5@users.noreply.github.com>
2024-12-20 16:27:24 +0300

    Update urls.py

--- messaging_app/messaging_app/urls.py
+++ messaging_app/messaging_app/urls.py
@@ -38,3 +38,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('messaging_app.chats.urls')),  # Including chat routes under 'api/' prefix
 ]
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('api/', include('messaging_app.chats.urls')),  # Include chats API routes
+    path('api-auth/', include('rest_framework.urls')),  # Add this line for authentication
+]
