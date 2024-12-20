dfa2cae220ccaf1302020267ef062bdd994b0ab4
sange5 <163669819+sange5@users.noreply.github.com>
2024-12-20 16:24:55 +0300

    Update urls.py

--- messaging_app/chats/urls.py
+++ messaging_app/chats/urls.py
@@ -8,5 +8,15 @@ router.register(r'messages', MessageViewSet, basename='message')
 
 # The router.urls automatically generates the URL patterns for these viewsets
 urlpatterns = router.urls
-
 ["from django.urls import", "path", "include", "routers.DefaultRouter()"]
+
+from rest_framework.routers import DefaultRouter
+from .views import ConversationViewSet, MessageViewSet
+
+# Create a router and register the viewsets
+router = DefaultRouter()
+router.register(r'conversations', ConversationViewSet, basename='conversation')
+router.register(r'messages', MessageViewSet, basename='message')
+
+# The router.urls automatically generates the URL patterns for these viewsets
+urlpatterns = router.urls
