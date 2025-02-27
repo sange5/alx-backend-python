094defafa5e29c1e9e3cf55f0df4cb65d7b5284b
sange5 <163669819+sange5@users.noreply.github.com>
2025-02-27 16:43:12 +0300

    Delete messaging_app directory

--- messaging_app/chats/auth.py
+++
@@ -1,5 +0,0 @@
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from rest_framework.permissions import AllowAny
-
-class CustomTokenObtainPairView(TokenObtainPairView):
-    permission_classes = [AllowAny]
--- messaging_app/chats/permissions.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.permissions import BasePermission
-
-class IsOwnerOrReadOnly(BasePermission):
-    """
-    Custom permission to allow users to access their own resources only.
-    """
-    def has_object_permission(self, request, view, obj):
-        return obj.user == request.user
